da58bd04e1f9b3341e6d269fd3db84c271dc4242
hunter <hunter.wxhu@gmail.com>
2013-06-29 14:40:37 +0800

    rails 4 with mongodb and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tongtu::Application.config.secret_key_base = '42a9498e38b25a9331944895fe4badd3a436da94c07cd81d5ac33e0ec45407d7b1509a559eaa9e9e129275625477074c4dcd1d6929e9a1ddd48105831491daa6'
